b5b87af4cf9876cb2d6ba52fea58422d3a0d8e32
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2019-10-06 12:48:44 -0300

    Configurando nodeMailer para cancelamento de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'be22f2f9a8b92c',
+    pass: '6b439b68b47cea',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
